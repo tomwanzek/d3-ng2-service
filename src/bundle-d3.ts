99fee5a46c1f5f789984176ab1d6af4fff687de2
merlosy <merlosy@users.noreply.github.com>
2017-01-27 12:02:55 +0100

    missing d3-request
    From what I noticed in d3 (v4.4.4), I think the `d3-request` is missing.
    Cheers

--- src/bundle-d3.ts
+++ src/bundle-d3.ts
@@ -18,6 +18,7 @@ export * from 'd3-polygon';
 export * from 'd3-quadtree';
 export * from 'd3-queue';
 export * from 'd3-random';
+export * from 'd3-request';
 export * from 'd3-scale';
 export * from 'd3-selection';
 export * from 'd3-selection-multi';
